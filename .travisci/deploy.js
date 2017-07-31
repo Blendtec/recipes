662fd027c6522c9a229e539319505bfc25beb37c
Stone C. Lasley <stonelasley@gmail.com>
2017-07-31 15:56:27 -0600

    chore(ci): add console to check if running

--- .travisci/deploy.js
+++ .travisci/deploy.js
@@ -1,15 +1,16 @@
-var fs = require('fs');
-var path = require('path');
-var ShopifyAPI = require('shopify-node-api');
+console.log("Deploying Application...");
+var fs = require("fs");
+var path = require("path");
+var ShopifyAPI = require("shopify-node-api");
 
 var pageId = process.env.SHOPIFY_PAGE_ID;
 var Shopify = new ShopifyAPI({
-  shop: process.env.SHOPIFY_SHOP,
-  shopify_api_key: process.env.SHOPIFY_API_KEY,
-  access_token: process.env.SHOPIFY_API_PASSWORD
+    shop: process.env.SHOPIFY_SHOP,
+    shopify_api_key: process.env.SHOPIFY_API_KEY,
+    access_token: process.env.SHOPIFY_API_PASSWORD
 });
 
-var indexPagePath = path.join(__dirname, '..', 'dist', 'index.html');
+var indexPagePath = path.join(__dirname, "..", "dist", "index.html");
 fs.readFile(indexPagePath, "utf-8", function (err, data) {
     if (!err) {
         var put_data = {
@@ -18,10 +19,14 @@ fs.readFile(indexPagePath, "utf-8", function (err, data) {
             }
         };
 
-        Shopify.put("/admin/pages/" + pageId + ".json", put_data, function(err, data, headers) {
-            console.log("ERROR", err);
-            console.log("PUT RESULT", data);
-            console.log("PUT HEADERS", headers);
+        Shopify.put("/admin/pages/" + pageId + ".json", put_data, function (err, data, headers) {
+            if (err) {
+                console.error("Failed to Deploy during API call");
+                return 0;
+            } else {
+                console.log("Deploy Success");
+                return 1;
+            }
         });
     }
 });
