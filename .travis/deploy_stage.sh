1f3c2b0c43eafadae46b8afa1d8480f9358179b1
Reid Kuttler <rkuttler@blendtec.com>
2017-08-03 11:23:51 -0600

    build(travis): seeing if the problem is I am trying to use stage bucket

--- .travis/deploy_stage.sh
+++ .travis/deploy_stage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Deploying to Shopify..."
 node ./.travis/shopify_stage.js
-npm run build:ci https://s3-us-west-1.amazonaws.com/recipe-stage-client.blendtec.com/
+npm run build:ci https://s3-us-west-1.amazonaws.com/recipe-client.blendtec.com/
 exit 0
